05d1a51ec99f6d96066f66318ff75fd8f0948459
Lon Bitner <lonbitner@gmail.com>
2020-04-22 22:09:11 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -25,7 +25,7 @@ def third_challenge
         name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => [
-        {name: "Steven", a}]
+        {name: "Steven", age: "30", atitude:}]
    }
   }
 
